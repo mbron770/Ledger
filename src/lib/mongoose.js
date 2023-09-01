53f1e12955854b3683fb7ee07d1beecb9c59f6ec
mbron770 <132225936+mbron770@users.noreply.github.com>
2023-08-31 21:12:41 -0400

    Update mongoose.js for deployment

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,23 +1,47 @@
-import mongoose from "mongoose";
-mongoose.set("debug", true);
+// import mongoose from "mongoose";
+// mongoose.set("debug", true);
 
-let isConnected = false;
+// let isConnected = false;
 
-export const connectToDB = async () => {
-  mongoose.set("strictQuery", true);
+// export const connectToDB = async () => {
+//   mongoose.set("strictQuery", true);
 
-  // if (!process.env.MONGODB_URI) return console.log("MONGODB_URL not found");
+//   // if (!process.env.MONGODB_URI) return console.log("MONGODB_URL not found");
 
-  if (isConnected) return console.log("Connected to MONGODB");
+//   if (isConnected) return console.log("Connected to MONGODB");
 
-  try {
-    await mongoose
-      .connect(process.env.MONGODB_URI)
-      .catch((error) => console.error("Database connection error:", error));
-    isConnected = true;
+//   try {
+//     await mongoose
+//       .connect(process.env.MONGODB_URI)
+//       .catch((error) => console.error("Database connection error:", error));
+//     isConnected = true;
 
-    console.log("2. Connect to Mongo");
-  } catch (error) {
-    console.log(error);
+//     console.log("2. Connect to Mongo");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+
+
+import mongoose from 'mongoose';
+
+const connection = {};
+
+async function connectToDB() {
+  if (connection.isConnected) {
+    return console.log('mongo connection established')
   }
-};
+
+  if (!connection.isConnected) {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    connection.isConnected = db.connections[0].readyState;
+  }
+}
+
+export default connectToDB
+
