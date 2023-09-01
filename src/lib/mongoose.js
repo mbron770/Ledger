3368b5de5072289b300618034b1d0958f32bf9a5
mbron770 <132225936+mbron770@users.noreply.github.com>
2023-08-31 20:34:05 -0400

    Update mongoose.js

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -6,7 +6,7 @@ let isConnected = false;
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URI) return console.log("MONGODB_URL not found");
+  if (!process.env.MONGODB_URI) return console.log("MONGODB_URI not found");
 
   if (isConnected) return console.log("Connected to MONGODB");
 
