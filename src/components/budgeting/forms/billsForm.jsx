64485f846c31e2e3314994512a5ab8395b60d78e
mbron770 <mbronfin.tsd@gmail.com>
2023-09-26 15:00:13 -0400

    bill form UI

--- src/components/budgeting/forms/billsForm.jsx
+++ src/components/budgeting/forms/billsForm.jsx
@@ -95,24 +95,24 @@ export default function AddBillsForm() {
         </button>
 
         {addBill && (
-                                        <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className="fixed top-20 left-0 right-0 bottom-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto flex items-center justify-center">
-                                            <div className="relative w-full max-w-md max-h-full">
-                                            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                        <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className="fixed inset-0 z-50 p-4 flex items-center justify-center md:px-8 lg:px-12">
+                                            <div className="relative w-full max-w-3xl max-h-full bg-custom-blue shadow overflow-y-auto">
+                                            <div className="relative bg-custom-blue  shadow ">
                                                 <button type="button"
                                                         onClick={toggleButton}
-                                                        className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                                                        className="absolute top-3 right-2.5 text-custom-purple bg-transparent hover:bg-gray-200 hover:text-gray-900 text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
                                                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                                         </svg>
                                                         <span className="sr-only">Close modal</span>
                                                     </button>
                                                     <div className="px-6 py-6 lg:px-8">
-                                                    <h3 className="mb-4 text-xl text-center font-medium text-gray-900 dark:text-white">Add your bills</h3>
-                                                    <form className="space-y-6" action="#" onSubmit={addBillToDB}>
+                                                    <h3 className="text-xl text-center font-medium text-custom-purple font-goldman">Add your bills</h3>
+                                                    <form className="space-y-6 md:space-y-0 grid  gap-6" action="#" onSubmit={addBillToDB}>
                                                     <div>
-                                                    <label for="incomeType" className="block  text-sm font-medium text-gray-900 dark:text-white">Select Bill Type</label>
+                                                    <label for="incomeType" className="block  text-sm font-medium text-custom-purple font-goldman mb-1">Select Bill Type</label>
                                                         <select id="incomeType" name='billType' value={billType} onChange={handleBillTypeChange}
-                                                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                         className="bg-custom-purple  text-white text-sm font-goldman  block w-full p-2.5 ">
                                                         <option>Utility</option>
                                                                 <option>Loan</option>
                                                                 <option>Subscription</option>
@@ -124,31 +124,31 @@ export default function AddBillsForm() {
                                                  
                         
                                                     <div>
-                                                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                                <label htmlFor="description" className="block  text-sm font-medium text-custom-purple font-goldman mb-1">
                                                                     Description
                                                                 </label>
                                                                 <input type="text" 
                                                                 id="description" 
                                                                 value={description}
                                                                 onChange={handleDescriptionChange}
-                                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Electric Bill" required/>
+                                                                className="bg-custom-purple  text-white text-sm font-goldman  block w-full p-2.5 " placeholder="Electric Bill" required/>
                                                             </div>
                             
                         
                                                     <div>
-                                                                <label htmlFor="company" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                                <label htmlFor="company" className="block  text-sm font-medium text-custom-purple font-goldman mb-1">
                                                                     Company
                                                                 </label>
                                                                 <input type="text"
                                                                 id="company"  
                                                                 value={company}
                                                                 onChange={handleCompanyChange}
-                                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="ConEdison" required/>
+                                                                className="bg-custom-purple  text-white text-sm font-goldman  block w-full p-2.5 " placeholder="ConEdison" required/>
                                                             </div>
                         <div>
-                                                            <label for="incomeType" className="block  text-sm font-medium text-gray-900 dark:text-white">How often is this bill due?</label>
+                                                            <label for="incomeType" className="block  text-sm font-medium text-custom-purple font-goldman mb-1">How often is this bill due?</label>
                                                                 <select id="payFrequency" value={payFrequency} onChange={handlePayFrequencyChange}
-                                                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                                className="bg-custom-purple  text-white text-sm font-goldman  block w-full p-2.5 "
                                                                 >
                                                                     <option value="weekly">Weekly</option>
                                                                     <option value="biweekly">Bi-Weekly</option>
@@ -158,14 +158,14 @@ export default function AddBillsForm() {
                                                             </div>
                             
                                                     <div>
-                                                    <label htmlFor="billTotal" className="block mt-2 text-sm font-medium text-gray-900 dark:text-white">How much is this bill?</label>
+                                                    <label htmlFor="billTotal" className="block  text-sm font-medium text-custom-purple font-goldman mb-1">How much is this bill?</label>
                                                         <input type="number" id="billTotal" value={billTotal} onChange={handleBillTotalChange} 
-                                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                                                        className="bg-custom-purple  text-white text-sm font-goldman  block w-full p-2.5 "
                                                         />
                                                     </div>
                             
                                                     <div>
-                                                    <button  type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                    <button  type="submit" className="w-full text-custom-blue font-goldman bg-custom-purple hover:bg-blue-300 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 text-center">
                                                                 Add Bill
                                                             </button>
                                                     </div>
