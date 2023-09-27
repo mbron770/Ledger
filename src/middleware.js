6852b6906e89efe926a64ae2eb029dcb0687bd10
Mordechai (Mo) Bronfin <132225936+mbron770@users.noreply.github.com>
2023-09-27 15:45:38 -0400

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -16,20 +16,20 @@ export const config = {
 };
 
 
-export function middleware(request){
-  const response = NextResponse.next();
+// export function middleware(request){
+//   const response = NextResponse.next();
 
-response.headers.set('Access-Control-Allow-Origin', '*')
-response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-response.headers.set('Access-Control-Allow-Origin', '86400')
-console.log('Middleware')
-console.log(request.method)
-console.log(request.url)
+// response.headers.set('Access-Control-Allow-Origin', '*')
+// response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+// response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+// response.headers.set('Access-Control-Allow-Origin', '86400')
+// console.log('Middleware')
+// console.log(request.method)
+// console.log(request.url)
 
-return response
+// return response
 
-}
+// }
 
 
 
